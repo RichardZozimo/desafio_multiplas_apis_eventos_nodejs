52d22b56d102f0d67d5eccd2de024a9d9553b144
Richard Zozimo <richardzozimo@gmail.com>
2021-11-28 18:19:34 -0300

    inicialiando api nodejs

---
+++ src/controllers/eventos_controller.js
@@ -0,0 +1,13 @@
+﻿const Evento = require('../models/evento')
+
+module.exports = class EventosController{
+  static index(req, res, next) {
+    res.status(200).send([
+      new Evento(),
+      new Evento(),
+      new Evento(),
+      new Evento(),
+      new Evento()
+    ]);
+  }
+}
